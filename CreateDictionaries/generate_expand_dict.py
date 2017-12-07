35066c0448095417f2cf94cdbb944dd19afc8a58
djyldyz <djyldyz@users.noreply.github.com>
2017-12-07 18:59:08 +0000

    Edited and re-run script to generate expand_dict.json.

--- CreateDictionaries/generate_expand_dict.py
+++ CreateDictionaries/generate_expand_dict.py
@@ -11,21 +11,21 @@ import pickle
 import os
 import json
 
+script_dir = os.path.dirname(__file__)
+parent_dir = os.path.join(script_dir, '..')
+lookup_dir = os.path.join(parent_dir, 'Dictionaries')
 
-directory = r"C:\Users\327660\Documents\Job_title_matching"
-directory.replace('\\','/')
-directory = os.path.normpath(directory)
-os.chdir(directory)
-
-#%% Create dictionary of abbreviations using manually curated csv
-expand_abbreviations = pd.read_csv('replace_lookup_abbreviations.csv', encoding = 'utf-8')
+# Create dictionary of abbreviations using manually curated csv
+expand_abbreviations = pd.read_csv(os.path.join(lookup_dir, 
+                                   'replace_lookup_abbreviations.csv'), 
+                                    encoding = 'utf-8')
 expand_dict = {}
 for ix, i in enumerate(expand_abbreviations.loc[:, 'Word']):
     expand_dict[i] = expand_abbreviations.loc[ix, 'Replace_with']
     
-#%% Write dictionary to pickle and json
-pickle.dump(expand_dict, open('expand_dict.pkl', 'wb'))
+# Write dictionary to pickle and json
+#pickle.dump(expand_dict, open(os.path.join(lookup_dir, 'expand_dict.pkl'), 'wb'))
 
-with open('expand_dict.json', 'w') as fp:
+with open(os.path.join(lookup_dir, 'expand_dict.json'), 'w') as fp:
     json.dump(expand_dict, fp, indent = 4)
     
